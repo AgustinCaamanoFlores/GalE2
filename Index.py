e8cda31a2e11384794b815f42f5963a48a571cdc
AgustinCaamanoFlores <54273840+AgustinCaamanoFlores@users.noreply.github.com>
2020-08-19 14:42:18 -0300

    IndexCreated
    Solo cree el index que contiene arrays para las distintas funciones

---
+++ Index.py
@@ -0,0 +1,10 @@
+class Index:
+    if __name__ == "__main__":
+        data0 = []
+        data1 = []
+        data2 = []
+        data3 = []
+        functionP2 = []
+        functionL = [[]]
+        functionH = []
+        pass
