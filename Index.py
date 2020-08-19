967f4e6fd2c3e30de1952b5b746ab5440fc9bc4c
AgustinCaamanoFlores <54273840+AgustinCaamanoFlores@users.noreply.github.com>
2020-08-19 15:45:26 -0300

    FunctionsCreated
    Funciones para obtener los valores de las funciones y cargar los datos en memoria creados

--- Index.py
+++ Index.py
@@ -1,10 +1,58 @@
+import math
 class Index:
+    gValues = [[]]
+    yValues = []
+    
     if __name__ == "__main__":
-        data0 = []
-        data1 = []
-        data2 = []
-        data3 = []
-        functionP2 = []
-        functionL = [[]]
-        functionH = []
+        
         pass
+
+    @staticmethod
+    def valueAtFuncton1(postion,variables):
+        y = (postion**2)*variables[2] + postion*variables[1] + variables[0]
+        return y
+
+    @staticmethod
+    def valueAtFunction2(postion,variables):
+        y = variables[0]*Index.getGValue(postion,0) + variables[1]*Index.getGValue(postion,1) + variables[2]*Index.getGValue(postion,2) + variables[3]*Index.getGValue(postion,3)
+        return y
+        
+    @staticmethod
+    def readGDataFromFile(path,position):
+        file = open(path,"r")
+        line = file.readline()
+        separetedValues = line.split("|")
+        count = 0
+        for x in separetedValues:
+            Index.gValues[position][count] = x
+            count += 1
+
+
+    @staticmethod
+    def getGValue(position,nfunction):
+        paths = []
+        if Index.gValues == None:
+            Index.readGDataFromFile(paths[nfunction],nfunction)
+        return Index.gValues[nfunction][position]
+        
+
+    @staticmethod
+    def valueAtFunction3(position,variables):
+        y = variables[0] + variables[1]*math.sin((math.pi/100)*position) + variables[2]*math.cos((math.pi/100)*position)
+        return y
+
+    @staticmethod
+    def readYDataFromFile():
+        file = open("path","r")
+        line = file.readline()
+        separetedValues = line.split("|")
+        count = 0
+        for x in separetedValues:
+            Index.yValues[count] = x
+            count += 1
+
+    @staticmethod
+    def distance(original,compared):
+        return (original-compared)
+
+
